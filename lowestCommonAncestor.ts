9fcc0fdc40d576ffced9a49f945d20f24d57c782
sam-ayo <sam-ayo>
2023-10-13 23:07:39 -0230

    day 7

---
+++ lowestCommonAncestor.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    if(root === null) return root;
+    if(p && q) {
+        if(p.val >= root.val && q.val >= root.val){
+           return lowestCommonAncestor(root.right, p, q);
+        }
+
+        if(p.val <= root.val && q.val <= root.val){
+           return lowestCommonAncestor(root.left, p, q);
+        }
+    }
+    return root;
+};
